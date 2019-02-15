dc9d75e943fa0693b22a265a2e084304fa0f54ef
Ashymad <szymon.mikulicz@posteo.net>
2019-02-15 23:37:25 +0100

    Sending interrupts successful!

--- src/slave.rs
+++ src/slave.rs
@@ -1,3 +1,16 @@
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::{Gpio, Trigger};
+
+
 pub fn main() {
-    println!("I'm a slave");
+    let gpio = Gpio::new().unwrap();
+    let mut pin = gpio.get(4).unwrap().into_input_pulldown();
+
+    pin.set_async_interrupt(Trigger::RisingEdge, |level| {
+        println!("Interrupted! Level: {}", level);
+    }).unwrap();
+
+    thread::sleep(Duration::from_secs(10));
 }
