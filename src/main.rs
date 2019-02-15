3e0be95350b801abbe4782cdb6fca72bb1084d9c
Ashymad <szymon.mikulicz@posteo.net>
2019-02-15 18:35:55 +0100

    Run module depending on argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate docopt;
+
+mod master;
+mod slave;
+
+use docopt::Docopt;
+
+const USAGE: &'static str = "
+Raspberry Pi Wave Field Synthesis System
+
+Usage:
+  wifs master
+  wifs slave
+  wifs (-h | --help)
+
+Options:
+  -h --help     Show this screen.
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    cmd_master: bool,
+    cmd_slave: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.deserialize())
+                            .unwrap_or_else(|e| e.exit());
+    if args.cmd_master {
+        master::main();
+    } else {
+        slave::main();
+    }
 }
---
+++ src/slave.rs
@@ -0,0 +1,3 @@
+pub fn main() {
+    println!("I'm a slave");
+}
