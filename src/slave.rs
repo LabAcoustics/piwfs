0c0566ef7e603fbaaa6cf8ad01242fcc4a841494
Ashymad <szymon.mikulicz@posteo.net>
2019-02-17 12:42:35 +0100

    Play 2 channels at once.

--- src/slave.rs
+++ src/slave.rs
@@ -1,4 +1,4 @@
-use std::sync::{Arc, Mutex};
+use std::sync::Mutex;
 
 use rppal::gpio::{Gpio, Trigger};
 use alsa::{Direction, ValueOr};
@@ -21,21 +21,20 @@ pub fn main(args : Args) {
     let mut pin = gpio.get(4).unwrap().into_input_pulldown();
 
     let pcm = PCM::new(&args.flag_device, Direction::Playback, false).unwrap();
-    let pcm_fd = Arc::new(Mutex::new(pcm_to_fd(&pcm).unwrap()));
 
     let guard = {
-        let pcm_fd = pcm_fd.clone();
+        let pcm_fd = Mutex::new(pcm_to_fd(&pcm).unwrap());
         pin.set_async_interrupt(Trigger::RisingEdge, move |_level| {
             let status = unsafe {
-                SyncPtrStatus::sync_ptr(*pcm_fd.lock().unwrap(), false, None, None).unwrap()
+                SyncPtrStatus::sync_ptr(*pcm_fd.lock().unwrap(), true, None, None).unwrap()
             };
-            println!("Interrupted! Time: {}:{}", status.htstamp().tv_sec, status.htstamp().tv_nsec);
+            println!("Time: {}:{}", status.htstamp().tv_sec, status.htstamp().tv_nsec);
         }).unwrap();
     };
 
     // Set hardware parameters: 44100 Hz / Mono / 16 bit
     let hwp = HwParams::any(&pcm).unwrap();
-    hwp.set_channels(1).unwrap();
+    hwp.set_channels(2).unwrap();
     hwp.set_rate(44100, ValueOr::Nearest).unwrap();
     hwp.set_format(Format::s16()).unwrap();
     hwp.set_access(Access::RWInterleaved).unwrap();
@@ -56,8 +55,8 @@ pub fn main(args : Args) {
     }
 
     // Play it back for 10 seconds.
-    for _ in 0..10*44100/1024 {
-        assert_eq!(io.writei(&buf[..]).unwrap(), 1024);
+    for _ in 0..10*44100/512 {
+        assert_eq!(io.writei(&buf[..]).unwrap(), 512);
     }
 
     // In case the buffer was larger than 2 seconds, start the stream manually.
