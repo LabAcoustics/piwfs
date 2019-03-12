2c116710523a6c3864acb0d6390b756cfd7ee8db
rotypo <marcel.piszak@wp.pl>
2019-03-12 22:22:01 +0100

    Preparation for network connection

--- src/master.rs
+++ src/master.rs
@@ -1,5 +1,8 @@
 use std::thread;
 use std::sync::mpsc::{self, TryRecvError};
+use mio::{Events, Poll, Ready, PollOpt, Token};
+use mio::net::TcpStream;
+use std::net::{TcpListener, SocketAddr};
 
 use rppal::gpio::Gpio;
 
@@ -10,8 +13,9 @@ pub fn main(args : Args) {
 
     let child = thread::spawn(move || {
         let gpio = Gpio::new().unwrap();
-        let mut pin = gpio.get(4).unwrap().into_output();
+        let mut pin = gpio.get(5).unwrap().into_output();
         let mut timer = adi_clock::Timer::new(0.005);
+	pin.set_reset_on_drop(false);
 
         loop {
             match rx.try_recv() {
@@ -21,9 +25,34 @@ pub fn main(args : Args) {
             timer.wait();
             pin.toggle();
         }
+	pin.set_high();
     });
+
+    let net_th = thread::spawn(move || {
+        let addr: SocketAddr = "0.0.0.0:10000".parse().unwrap();
+        let server = TcpListener::bind(&addr).unwrap();
+        let poll = Poll::new().unwrap();
+        let mut events = Events::with_capacity(1024);
+        let stream = TcpStream::connect(&server.local_addr().unwrap()).unwrap();
+
+        poll.register(&stream, Token(0), Ready::readable() | Ready::writable(), PollOpt::edge()).unwrap();
+
+        loop {
+            poll.poll(&mut events, None).unwrap();
+
+            for event in &events {
+                if event.token() == Token(0) && event.readiness().is_writable() {
+
+                    
+                    break;
+                }
+            }
+        }
+    });
+
 
     thread::sleep(std::time::Duration::new(1, 0));
     tx.send(()).unwrap();
-    child.join().unwrap();;
+    net_th.join().unwrap();
+    child.join().unwrap();
 }
