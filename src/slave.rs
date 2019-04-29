5e9bed7dbe82d21532440d9f6cd987da327e106d
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2019-04-30 00:52:54 +0200

    Use poll() instead if interrupts; read /dev/rfrpi for deviation.

--- src/slave.rs
+++ src/slave.rs
@@ -2,28 +2,21 @@ use std::sync::{Arc, Mutex, Barrier};
 use std::sync::mpsc::{self, TryRecvError, Receiver};
 use std::io::Write;
 use std::thread;
+use std::fs::File;
+use std::io::prelude::*;
+use std::os::unix::io::AsRawFd;
 
-use rppal::gpio::{Gpio, Trigger, InputPin};
 use alsa::{Direction, ValueOr};
 use alsa::pcm::{PCM, HwParams, Format, Access, State};
-use alsa::direct::pcm::SyncPtrStatus;
 use num::pow;
 use ta::indicators::SimpleMovingAverage;
 use ta::Next;
 use samplerate::{Samplerate, ConverterType};
 use hound;
 use thread_priority::{ThreadPriority, ThreadSchedulePolicy, NormalThreadSchedulePolicy};
+use nix::poll::*;
 
 use super::Args;
-
-fn pcm_to_fd(p: &PCM) -> alsa::Result<std::os::unix::io::RawFd> {
-    let mut fds: [libc::pollfd; 1] = unsafe { std::mem::zeroed() };
-    let c = (p as &alsa::PollDescriptors).fill(&mut fds)?;
-    if c != 1 {
-        return Err(alsa::Error::unsupported("snd_pcm_poll_descriptors returned wrong number of fds"))
-    }
-    Ok(fds[0].fd)
-}
 
 fn vec_f32_to_i16(vec: &[f32]) -> Vec<i16> {
     return vec.into_iter().map(|&e| {
@@ -31,48 +24,41 @@ fn vec_f32_to_i16(vec: &[f32]) -> Vec<i16> {
     }).collect();
 }
 
-fn synch_status(pin: &mut InputPin, pcm_fd: &std::os::unix::io::RawFd, sma_val: &Arc<Mutex<f64>>,
-                int_counter: &Arc<Mutex<u64>>, int_time: u32, rx: &Receiver<u64>, sma_num: u32,
-                barrier: &Arc<Barrier>) {
-    thread_priority::set_thread_priority(thread_priority::thread_native_id(),
-        ThreadPriority::Max,
-        ThreadSchedulePolicy::Normal(NormalThreadSchedulePolicy::Normal)).unwrap();
+fn synch_status(sma_val: &Arc<Mutex<f64>>, int_counter: &Arc<Mutex<u64>>,
+                int_time: u32, rx: &Receiver<u64>, sma_num: u32, barrier: &Arc<Barrier>) {
+    thread_priority::set_thread_priority(thread_priority::thread_native_id(), ThreadPriority::Max,
+                ThreadSchedulePolicy::Normal(NormalThreadSchedulePolicy::Normal)).unwrap();
     let mut sma = SimpleMovingAverage::new(sma_num).unwrap();
     let mut counter = 0;
     let mut next_barrier = 0;
-    let max_dev = (int_time / 1000) as i32;
-    let mut prev_time: u64 = 0;
-    pin.set_interrupt(Trigger::RisingEdge).unwrap();
+    let mut rfrpi = match File::open("/dev/rfrpi") {
+        Ok(res) => res,
+        Err(err) => panic!("Couldn't open /dev/rfrpi: {}", err),
+    };
+    let rfrpi_pfd = PollFd::new(rfrpi.as_raw_fd(), EventFlags::POLLIN);
+    let mut int_times_buf = [0; 5000];
+    if let Err(err) = rfrpi.read(&mut int_times_buf) {
+        panic!("Couldn't read from /dev/rfrpi: {}", err);
+    }
     loop {
-        match pin.poll_interrupt(true, None) {
+        match poll(&mut [rfrpi_pfd], -1) {
             Ok(_) => {
-                match unsafe { SyncPtrStatus::sync_ptr(*pcm_fd, true, None, None) } {
-                    Ok(status) => {
-                        counter += 1;
-                        if let Ok(mut val) = int_counter.try_lock() {
-                            *val = counter;
-                        }
-                        if counter == next_barrier { barrier.wait(); }
-                        let cur_time = status.htstamp().tv_sec as u64 * pow::pow(10u64,9) + status.htstamp().tv_nsec as u64;
-                        if prev_time != 0 {
-                            let dev = int_time as i32 - (cur_time - prev_time) as i32;
-                            if dev.abs() < max_dev {
-                                let next_val = sma.next(dev as f64);
-                                if let Ok(mut val) = sma_val.try_lock() {
-                                    *val = next_val;
-                                }
-                            }
-                        }
-                        prev_time = cur_time;
-                    }
-                    Err(e) => {
-                        if let Ok(0) = rx.try_recv() {
-                            break;
-                        } else {
-                            panic!("Error syncing pointer: {:?}", e);
-                        }
-                    }
+                let int_times_str = match rfrpi.read(&mut int_times_buf) {
+                    Ok(size) => String::from_utf8(int_times_buf[..size].into()).unwrap(),
+                    Err(err) => panic!("Couldn't read from /dev/rfrpi: {}", err),
+                };
+                counter += 1;
+                if let Ok(mut val) = int_counter.try_lock() {
+                    *val = counter;
                 }
+                if counter == next_barrier { barrier.wait(); }
+                int_times_str.lines().for_each(|line| {
+                    let dev = int_time as i32 - i32::from_str_radix(&line, 10).unwrap();
+                    let next_val = sma.next(dev as f64);
+                    if let Ok(mut val) = sma_val.try_lock() {
+                        *val = next_val;
+                    }
+                });
             }
             Err(_) => panic!("Error polling interrupt!")
         }
@@ -85,9 +71,6 @@ fn synch_status(pin: &mut InputPin, pcm_fd: &std::os::unix::io::RawFd, sma_val: 
 }
 
 pub fn main(args: Args) {
-    let gpio: rppal::gpio::Gpio = Gpio::new().unwrap();
-    let mut pin: InputPin = gpio.get(16).unwrap().into_input_pullup();
-
     let pcm = PCM::new(&args.flag_device, Direction::Playback, false).unwrap();
 
     let (tx, rx) = mpsc::channel();
@@ -104,12 +87,11 @@ pub fn main(args: Args) {
     let int_counter = Arc::new(Mutex::new(0u64));
     let barrier = Arc::new(Barrier::new(2));
     let sync_thr = {
-        let pcm_fd = pcm_to_fd(&pcm).unwrap();
         let sma_val = sma_val.clone();
         let int_counter = int_counter.clone();
         let barrier = barrier.clone();
         thread::spawn(move || {
-            synch_status(&mut pin, &pcm_fd, &sma_val, &int_counter, int_time, &rx, sma_num, &barrier)
+            synch_status(&sma_val, &int_counter, int_time, &rx, sma_num, &barrier)
         })
     };
 
@@ -126,7 +108,6 @@ pub fn main(args: Args) {
     let period_size = hwp.get_period_size().unwrap();
     let buffer_size = hwp.get_buffer_size().unwrap();
     swp.set_start_threshold(buffer_size).unwrap();
-    swp.set_tstamp_mode(true).unwrap();
     pcm.sw_params(&swp).unwrap();
 
     let sam_num = period_size as usize * num_channels;
@@ -148,12 +129,8 @@ pub fn main(args: Args) {
         if first_time {
             first_time = false;
             let wait_for = sma_num as u64;
-            let zeros = vec![0i16; sam_num];
-            assert_eq!(io.writei(&zeros).unwrap(), zeros.len()/num_channels);
-            pcm.start().unwrap();
             if args.flag_verbose { println!("Measuring deviation..."); }
             while *int_counter.lock().unwrap() < wait_for {
-                assert_eq!(io.writei(&zeros).unwrap(), zeros.len()/num_channels);
                 if args.flag_verbose {
                     let dev = *sma_val.lock().unwrap() as i32;
                     print!("Deviation: {} ns \r", dev);
@@ -161,8 +138,6 @@ pub fn main(args: Args) {
                 }
             }
             let dev = *sma_val.lock().unwrap() as i32;
-            pcm.drop().unwrap();
-            pcm.prepare().unwrap();
             converter = Samplerate::new(ConverterType::Linear,
                                         int_time, (int_time as i32 + dev) as u32,
                                         num_channels).unwrap();
